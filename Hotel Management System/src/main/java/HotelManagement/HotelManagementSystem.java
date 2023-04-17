1e7f4745e3b66aa6f20d674a5e29e5226cdeee0d
CucosMarianita <99651649+CucosMarianita@users.noreply.github.com>
2023-04-17 17:49:43 +0300

    Add files via upload

---
+++ Hotel Management System/src/main/java/HotelManagement/HotelManagementSystem.java
@@ -0,0 +1,49 @@
+package HotelManagement;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class HotelManagementSystem extends JFrame implements ActionListener {
+
+    HotelManagementSystem(){
+
+        setBounds(150,50,1100,700);
+        setLayout(null);
+
+        ImageIcon image_hotel = new ImageIcon(getClass().getResource("hotel.jpg"));
+        JLabel image = new JLabel(image_hotel);
+        image.setBounds(0,0,1100,700);
+        add(image);
+
+        JLabel text = new JLabel("Welcome!");
+        text.setBounds(100,40,1000,90);
+        text.setForeground(Color.WHITE);
+        text.setFont(new Font("serif", Font.PLAIN, 70));
+        image.add(text);
+
+        JButton next = new JButton("Next");
+        next.setBounds(850, 550, 150, 50);
+        next.setBackground(Color.WHITE);
+        next.setForeground(Color.BLACK);
+        next.addActionListener(this);
+        next.setFont(new Font("serif", Font.PLAIN, 24));
+        image.add(next);
+
+        setVisible(true);
+
+    }
+
+
+    public static void main(String[] args) {
+        new HotelManagementSystem();
+
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        setVisible(false);
+        new Login();
+    }
+}
---
+++ Hotel Management System/src/main/java/HotelManagement/Login.java
@@ -0,0 +1,56 @@
+package HotelManagement;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Login extends JFrame {
+
+    Login(){
+        getContentPane().setBackground(Color.WHITE);
+
+        setLayout(null);
+
+        JLabel user = new JLabel("Username");
+        user.setBounds(40, 20, 100, 30);
+        add(user);
+
+        JTextField username = new JTextField();
+        username.setBounds(150, 20, 150, 30);
+        add(username);
+
+        JLabel pass = new JLabel("Password");
+        pass.setBounds(40, 70, 100, 30);
+        add(pass);
+
+        JTextField password = new JTextField();
+        password.setBounds(150, 70, 150, 30);
+        add(password);
+
+        JButton login = new JButton("Login");
+        login.setBounds(40, 150, 120, 30);
+        login.setBackground(Color.BLACK);
+        login.setForeground(Color.WHITE);
+        add(login);
+
+        JButton cancel = new JButton("Cancel");
+        cancel.setBounds(180, 150, 120, 30);
+        cancel.setBackground(Color.BLACK);
+        cancel.setForeground(Color.WHITE);
+        add(cancel);
+
+        ImageIcon image_login = new ImageIcon(getClass().getResource("login2.png"));
+        Image i2 = image_login.getImage().getScaledInstance(200, 200, Image.SCALE_DEFAULT);
+        ImageIcon i3 = new ImageIcon(i2);
+        JLabel image = new JLabel(i3);
+        image.setBounds(350, 10, 200, 200);
+        add(image);
+
+
+        setBounds(500, 200, 600, 300);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new Login();
+    }
+}
